3db635f9b8c2b7ddfd8f2de7d2165eaa8cc5210f
Merjen Charyyeva <merjen.chary@gmail.com>
2019-03-21 19:38:34 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class CChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
   def change
     change_column :students, :birthday, :datetime 
   end
